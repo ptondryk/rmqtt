2c8d44f5ce39b1bfef54cc9c85405678012a2f7d
Pawel Tondryk <ptondryk@ymail.com>
2016-01-25 00:19:31 +0100

    - it is possible now to lock the main thread and only let the mqtt-client read the messages from subscribed topics

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::net::TcpStream;
 use std::thread;
 use std::sync::{Mutex, Arc};
 use std::str;
+use std::thread::JoinHandle;
+use std::time::Duration;
 
 use mqtt::*;
 mod mqtt;
@@ -12,58 +14,85 @@ trait HandlesMessage {
 }
 
 struct Mqtt {
-    host: String,
-    user: String,
-    password: String,
-    packet_id: i16,
-    stream: Mutex<Option<TcpStream>>
+    join_handler: JoinHandle<()>,
+    connection_wrapper: Arc<Mutex<MqttConnection>>
 }
 
 impl Mqtt {
-    fn new(host: &str, user: &str, password: &str) -> Mqtt {
+
+    fn connect(address: &str, settings: Option<MqttConnectionSettings>) -> Mqtt {
+        let (mqtt_connection, join_handler) = MqttConnection::connect(address, settings);
         Mqtt {
-            host: host.to_string(),
-            user: user.to_string(),
-            password: password.to_string(),
-            packet_id: 0,
-            stream: Mutex::new(None)
+            connection_wrapper: mqtt_connection,
+            join_handler: join_handler
         }
     }
 
-    fn connect(&mut self) {
-        // connect to the broker-server
-        let stream = Arc::new(Mutex::new(Some(TcpStream::connect(&*self.host).unwrap())));
-        self.create_connection(stream.clone());
+    pub fn read_forever(self) {
+        self.join_handler.join();
+    }
+
+    fn subscribe<T: HandlesMessage>(&mut self, topic: &str, handler: T) {
+        self.connection_wrapper.lock().unwrap().subscribe(topic, handler);
+    }
+}
+
+struct MqttConnection {
+    stream: Option<TcpStream>,
+    packet_id: i16
+}
+
+impl MqttConnection {
+
+    fn connect(host: &str, settings: Option<MqttConnectionSettings>)
+            -> (Arc<Mutex<MqttConnection>>, JoinHandle<()>) {
+        let stream = TcpStream::connect(host).unwrap();
+        let mut mqtt_connection = Arc::new(
+                        Mutex::new(MqttConnection {
+                                packet_id: 0,
+                                stream: Some(stream)
+                            }
+                        ));
+
+        // start the receive-thread
+        let mut child_mqtt_connection = mqtt_connection.clone();
+        let join_handler = thread::spawn(move || {
+            loop {
+                child_mqtt_connection.lock().unwrap().receive();
+            }
+        });
+
+        let shared_mqtt_connection = mqtt_connection.clone();
 
         // send CONNECT packet to mqtt-broker
-        self.send(&CONNECT::new_with_authentication("testClientId", "system", "manager")
-                    .as_bytes().into_boxed_slice());
+        match settings {
+            Some(mqtt_settings) => {
+                shared_mqtt_connection.lock().unwrap().send(&CONNECT::new_with_authentication("testClientId",
+                    &mqtt_settings.user, &mqtt_settings.password).as_bytes().into_boxed_slice());
+            }, None => {}
+        }
 
-        // start receive thread
-        thread::spawn(move || {
-            self.receive();
-        });
+        // return the connection object and the join handle
+        (shared_mqtt_connection, join_handler)
     }
 
-    fn create_connection(&mut self, stream: Mutex<Option<TcpStream>>) {
-        self.stream = stream;
-    }
 
-    fn publish(&mut self, topic: &str, text: &str) -> bool {
+    fn publish(&mut self, topic: &str, payload: &str) -> bool {
         unimplemented!()
     }
 
     fn subscribe<T: HandlesMessage>(&mut self, topic: &str, handler: T) -> bool {
         // send SUBSCRIBE packet to mqtt-broker
-        self.send(&SUBSCRIBE::new(topic, 0, self.packet_id).as_bytes().into_boxed_slice());
+        let new_packet_id = self.packet_id;
+        self.send(&SUBSCRIBE::new(topic, 0, new_packet_id).as_bytes().into_boxed_slice());
         self.packet_id = self.packet_id + 1;
         true
     }
 
-    fn send(&self, bytes: &[u8]) -> bool {
-        match self.stream.lock() {
-            Some(ref valid_stream) => {
-                let _ = valid_stream.write(bytes);
+    fn send(&mut self, bytes: &[u8]) -> bool {
+        match self.stream {
+            Some(ref mut tcp_stream) => {
+                let _ = tcp_stream.write(bytes);
                 true
             },
             None => {
@@ -73,30 +102,33 @@ impl Mqtt {
         }
     }
 
-    fn receive(&self) {
-        println!("start receive thread");
-
-        let mut buffer: Vec<u8> = Vec::new();
-        loop {
-            match self.stream.lock() {
-                Some(ref valid_stream) => {
-                    for byte in valid_stream.bytes() {
-                        buffer.push(byte.unwrap());
-                        let received_packet = mqtt::parse(&buffer);
-                        match received_packet {
-                            Some(ref packet) => {
-                                println!("parsed packet");
-                                println!("{:?}", packet);
-                                buffer.clear();
-                            },
-                            None => {}
-                        }
+    fn receive(&mut self) {
+        match(self.stream) {
+            Some(ref mut tcp_stream) => {
+                let mut buffer: Vec<u8> = Vec::new();
+                for byte in tcp_stream.bytes() {
+                    buffer.push(byte.unwrap());
+                    let received_packet = mqtt::parse(&buffer);
+                    match received_packet {
+                        Some(ref packet) => {
+                            println!("parsed packet");
+                            println!("{:?}", packet);
+                            buffer.clear();
+                        },
+                        None => {}
                     }
-                },
-                None => {}
-            }
+                }
+            },
+            None => {}
         }
+        thread::sleep(Duration::from_millis(3000));
     }
+
+}
+
+struct MqttConnectionSettings {
+    user: String,
+    password: String
 }
 
 struct ExampleHandler {
@@ -114,8 +146,10 @@ impl HandlesMessage for ExampleHandler {
 }
 
 fn main() {
-    let mut m = Mqtt::new("localhost:1883", "host", "password");
-    m.connect();
+    let mut m = Mqtt::connect("localhost:1883", Some(MqttConnectionSettings {
+                                                    user: "system".to_string(),
+                                                    password: "manager".to_string()
+                                                }));
     m.subscribe("testTopic1", ExampleHandler {
         example_var: 1
     });
