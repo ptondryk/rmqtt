7c02e3a2e34d8753cf515b0dbc371acd23959b8f
Pawel Tondryk <ptondryk@ymail.com>
2016-04-26 02:29:44 +0200

    tests os independant

--- tests/lib.rs
+++ tests/lib.rs
@@ -4,20 +4,17 @@ use rmqtt::MqttSessionBuilder;
 use rmqtt::ConnectFailed;
 
 #[test]
+#[allow(unused_variables)]
 fn test_connection_refused() {
     match MqttSessionBuilder::new("test-client-01", "localhost:1884")
             .credentials("user", "password")
             .keep_alive(120)
             .connect() {
         Err(error_type) => {
-            match error_type {
-                ConnectFailed::ConnectionError { details } => {
-                    assert_eq!("Connection refused (os error 61)", details);
-                }, _ => {
-                    // ConnectionError should occure
-                    assert!(false);
-                }
-            }
+			match error_type {
+				ConnectFailed::ConnectionError { details } => {},
+				_ => { assert!(false); }
+			}
         },
         _ => {
             // Err should occure
